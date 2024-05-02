48b7dc510af42a8c880126716d8fba1552162b07
cyndimustapha <162134378+cyndimustapha@users.noreply.github.com>
2024-05-02 11:47:47 +0300

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,28 +1,38 @@
-//SearchBar.jsx
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { FaSearch } from 'react-icons/fa';
 
 function SearchBar({ onSearch }) {
   const [searchTerm, setSearchTerm] = useState("");
 
   function handleChange(event) {
-    setSearchTerm(event.target.value);
+    const term = event.target.value;
+    setSearchTerm(term); // Update the searchTerm state
+    onSearch(term); // Trigger the search with the current value of searchTerm
   }
-
-  const handleSearch = () => {
-    onSearch(searchTerm);
-  };
 
   return (
     <div>
-      <input style={{marginRight: '20px', marginBottom: '10px', padding: '10px', borderRadius: '5px', width: '90%'}}
+      <input 
+        style={{
+          marginRight: '20px',
+          marginBottom: '10px',
+          padding: '10px',
+          borderRadius: '5px',
+          width: '90%'
+        }}
         type="text"
         value={searchTerm}
         onChange={handleChange}
         placeholder="Enter search term..."
       />
-      <button onClick={handleSearch}
-      style={{padding: '10px', color: 'black', cursor: 'pointer', borderRadius: '5px'}}>
+      <button 
+        style={{
+          padding: '10px',
+          color: 'black',
+          cursor: 'pointer',
+          borderRadius: '5px'
+        }}
+      >
         <FaSearch />
       </button>
     </div>
